8205646ef85b8f9842d584810679e0a3c0ac0c07
Daumantas Jakas <donnervetter1@gmail.com>
2021-11-17 17:10:26 +0200

    Strings translate added

--- src/components/RenderSchemaModal.js
+++ src/components/RenderSchemaModal.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Modal, Button } from "@shopify/polaris";
 import "../css/styles.css";
 import PropTypes from "prop-types";
+import { cloneDeep } from "lodash";
 
 class RenderFieldModal extends Component {
   state = {
@@ -17,11 +18,56 @@ class RenderFieldModal extends Component {
     this.setState(({ modalActive }) => ({ modalActive: !modalActive }));
   };
 
+  addTransformStrings = (reorderedObject, fields) => {
+    let objectWithTranslationStrings = cloneDeep(reorderedObject);
+
+    // updating translation strings
+    if (reorderedObject.name && reorderedObject.name.length > 0) {
+      objectWithTranslationStrings.name = `t:sections.${reorderedObject.name}.name`;
+    }
+
+    // settings
+    objectWithTranslationStrings.settings.map((settingsItem, index) => {
+      // label
+      if (settingsItem.label.length > 0) {
+        objectWithTranslationStrings.settings[index].label = `t:sections.${fields.store.name}.settings.${settingsItem.id}.label`;
+      }
+      // info
+      if (settingsItem.info.length > 0) {
+        objectWithTranslationStrings.settings[index].info = `t:sections.${fields.store.name}.settings.${settingsItem.id}.info`;
+      }
+      // placeholder
+      if (settingsItem.placeholder.length > 0) {
+        objectWithTranslationStrings.settings[index].placeholder = `t:sections.${fields.store.name}.settings.${settingsItem.id}.placeholder`;
+      }
+    });
+
+    // blocks
+    if (objectWithTranslationStrings.blocks) {
+      objectWithTranslationStrings.blocks.map((blockItem) => {
+        blockItem.settings.map((blockItemSettingsItem) => {
+          // label
+          if (blockItemSettingsItem.label.length > 0) {
+            blockItemSettingsItem.label = `t:sections.${fields.store.name}.blocks.${blockItemSettingsItem.id}.label`;
+          }
+          // info
+          if (blockItemSettingsItem.info && blockItemSettingsItem.info.length > 0) {
+            blockItemSettingsItem.info = `t:sections.${fields.store.name}.blocks.${blockItemSettingsItem.id}.info`;
+          }
+          // placeholder
+          if (blockItemSettingsItem.placeholder.length > 0) {
+            blockItemSettingsItem.placeholder = `t:sections.${fields.store.name}.blocks.${blockItemSettingsItem.id}.placeholder`;
+          }
+        });
+      });
+    }
+
+    return objectWithTranslationStrings;
+  };
+
   getFieldJSON = () => {
     const removeQuotesRegex = new RegExp(/"(min|max|step)": "(\d*)"/gi);
     const { fields, settings, blocks } = this.props;
-
-    console.log("get field json called", this.props.settings);
 
     const reorderedBlocks =
       blocks.length === 0
@@ -43,11 +89,9 @@ class RenderFieldModal extends Component {
       ...(blocks.length > 0 && { blocks: reorderedBlocks, presets: [{ name: fields.store.name, blocks: [...presets] }] }),
     };
 
-    // name: fields.store.name
+    let transformedItems = this.addTransformStrings(reorderedObject, fields);
 
-    let stringifiedFieldItems = JSON.stringify(reorderedObject, null, 2).replace(removeQuotesRegex, '"$1": $2');
-
-    console.log("stringified ites: ", stringifiedFieldItems);
+    let stringifiedFieldItems = JSON.stringify(transformedItems, null, 2).replace(removeQuotesRegex, '"$1": $2');
 
     stringifiedFieldItems = `{% schema %}\n` + stringifiedFieldItems + `\n{% endschema %}`;
 
